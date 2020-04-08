cd5cb902dbfc32e7b3f29dccc8aa311c6aae060c
jwhendy <jw.hendy@gmail.com>
2020-04-08 13:38:56 -0500

    updates to mobility data, added time series for all US counties

--- lib/mobility-script.sh
+++ lib/mobility-script.sh
@@ -4,7 +4,6 @@ PATH=/home/jwhendy/vault/personal/covid19
 cd $PATH/mobility-report-data-extractor
 
 while read code; do
-  #/usr/bin/python ./mobius.py download -p $code
-  /usr/bin/python ./mobius.py proc svgs/${code}.svg ./output
-  #/usr/bin/python ./mobius.py full pdfs/${code}.pdf svgs/${code}.svg ./output
+  /usr/bin/python ./mobius.py download $code
+  /usr/bin/python ./mobius.py proc svgs/${code}.svg ./output ./config/dates_lookup.csv
 done < $PATH/lib/mobility-code-list.txt
