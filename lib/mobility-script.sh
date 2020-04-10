b56a2b9b5e474df4d105ff4ef9b32050137f194b
jwhendy <jw.hendy@gmail.com>
2020-04-10 14:39:49 -0500

    updated mobility data to 2020-04-05, upstream fixes now incorporated which addresses some glitches in this data

--- lib/mobility-script.sh
+++ lib/mobility-script.sh
@@ -4,6 +4,6 @@ PATH=/home/jwhendy/vault/personal/covid19
 cd $PATH/mobility-report-data-extractor
 
 while read code; do
-  /usr/bin/python ./mobius.py download $code
-  /usr/bin/python ./mobius.py proc svgs/${code}.svg ./output ./config/dates_lookup.csv
+#  /usr/bin/python ./mobius.py download $code
+  /usr/bin/python ./mobius.py proc svgs/${code}.svg ./output ./config/dates_lookup_2020_04_05.csv
 done < $PATH/lib/mobility-code-list.txt
