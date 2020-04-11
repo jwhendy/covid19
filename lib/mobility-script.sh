f78d5bbc6a86b62d3b3b87339a330809aa5729b2
jwhendy <jw.hendy@gmail.com>
2020-04-11 15:29:36 -0500

    split up mobility data by dates, merged time-series mobility data into one

--- lib/mobility-script.sh
+++ lib/mobility-script.sh
@@ -2,8 +2,9 @@
 
 PATH=/home/jwhendy/vault/personal/covid19
 cd $PATH/mobility-report-data-extractor
+date='2020-04-05'
 
 while read code; do
-#  /usr/bin/python ./mobius.py download $code
-  /usr/bin/python ./mobius.py proc svgs/${code}.svg ./output ./config/dates_lookup_2020_04_05.csv
+  # /usr/bin/python ./mobius.py download $code $date
+  /usr/bin/python ./mobius.py proc svgs/${code}_${date}.svg ./output ./config/dates_lookup_2020_04_05.csv
 done < $PATH/lib/mobility-code-list.txt
