ef4f90fa11ea694508644a893b3b62fa1dd2316d
jwhendy <jw.hendy@gmail.com>
2020-04-12 18:17:38 -0500

    redo of mobility data using mobius.py full option; added errata files to track mismatched ts vs. agg data

--- lib/mobility-script.sh
+++ lib/mobility-script.sh
@@ -2,13 +2,15 @@
 
 PATH=/home/jwhendy/vault/personal/covid19
 cd $PATH/mobility-report-data-extractor
-Y='2020'
-M='03'
-D='29'
+Y="2020"
+M="04"
+D="05"
 date=${Y}-${M}-${D}
+
+echo $date
 
 while read code; do
   #/usr/bin/python ./mobius.py download $code $date
   #/usr/bin/python ./mobius.py proc svgs/${code}_${date}.svg ./output ./config/dates_lookup_2020_04_05.csv
-  /usr/bin/python ./mobius.py full -d ./config/dates_lookup_${Y}_${M}_${D} pdfs/${code}_${date}.pdf svgs/${code}_${date}.svg ./output
+  echo "/usr/bin/python ./mobius.py full -d ./config/dates_lookup_${Y}_${M}_${D}.csv pdfs/${code}_${date}.pdf svgs/${code}_${date}.svg ./output"
 done < $PATH/lib/mobility-code-list.txt
