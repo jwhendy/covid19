01bfb5f641fa544e73407712263986ea1980c490
jwhendy <jw.hendy@gmail.com>
2020-04-12 11:30:00 -0500

    about to switch from homebrew to mobius.py full option

--- lib/mobility-script.sh
+++ lib/mobility-script.sh
@@ -2,9 +2,13 @@
 
 PATH=/home/jwhendy/vault/personal/covid19
 cd $PATH/mobility-report-data-extractor
-date='2020-04-05'
+Y='2020'
+M='03'
+D='29'
+date=${Y}-${M}-${D}
 
 while read code; do
-  # /usr/bin/python ./mobius.py download $code $date
-  /usr/bin/python ./mobius.py proc svgs/${code}_${date}.svg ./output ./config/dates_lookup_2020_04_05.csv
+  #/usr/bin/python ./mobius.py download $code $date
+  #/usr/bin/python ./mobius.py proc svgs/${code}_${date}.svg ./output ./config/dates_lookup_2020_04_05.csv
+  /usr/bin/python ./mobius.py full -d ./config/dates_lookup_${Y}_${M}_${D} pdfs/${code}_${date}.pdf svgs/${code}_${date}.svg ./output
 done < $PATH/lib/mobility-code-list.txt
